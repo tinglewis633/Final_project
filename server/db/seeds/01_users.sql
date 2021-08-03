44e402fc5739f8be51f94033bf61c9907220b0db
Lewis <72407518+tinglewis633@users.noreply.github.com>
2021-08-03 23:16:15 +0800

    remove username for now so that user auth doesnt break

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (name, username, password, email, date_of_birth) VALUES ('lewis', 'lewistheG', 'lewis123', 'lewis@123.com', '1999-01-01');
-INSERT INTO users (name, username, password, email, date_of_birth) VALUES ('saka', 'saka.1K', 'saka123', 'saka@123.com', '1993-03-03');
-INSERT INTO users (name, username, password, email, date_of_birth) VALUES ('roddy', 'prince', 'roddyman', 'rod@123.com', '2000-11-01');
-INSERT INTO users (name, username, password, email, date_of_birth) VALUES ('benji', '1SA', 'benjiboy', 'benjamin@gmail.com', '1995-04-03');
+INSERT INTO users (name, password, email, date_of_birth) VALUES ('lewis', 'lewis123', 'lewis@123.com', '1999-01-01');
+INSERT INTO users (name, password, email, date_of_birth) VALUES ('saka', 'saka123', 'saka@123.com', '1993-03-03');
+INSERT INTO users (name, password, email, date_of_birth) VALUES ('roddy', 'roddyman', 'rod@123.com', '2000-11-01');
+INSERT INTO users (name, password, email, date_of_birth) VALUES ('benji', 'benjiboy', 'benjamin@gmail.com', '1995-04-03');
